270fcc93333632d833c49d46d311aa134f1168b9
Ali Houshangi <blumy.info@gmail.com>
2025-07-24 01:35:42 +0330

    layout in desktop done

--- src/components/ui/Social/Social.tsx
+++ src/components/ui/Social/Social.tsx
@@ -1,5 +1,5 @@
-import FacebookIcon from "@/assets/icons/FaceBook.webp";
 import GithubIcon from "@/assets/icons/Github.webp";
+import InstagramIcon from "@/assets/icons/Instagram.webp";
 import MailIcon from "@/assets/icons/MailIcon.webp";
 import Image from "next/image";
 import Link from "next/link";
@@ -11,7 +11,7 @@ const Social = () => {
         href="mailto:aliiihoushangiii@gmail.com"
         rel="noopener noreferrer"
         aria-label="send me a message in email"
-        className="w-[55%] relative z-20 bg-[#143061] rounded-[35px] p-6 overflow-hidden flex flex-col gap-10"
+        className="w-[52%] relative z-20 bg-[#143061] rounded-[35px] p-6 overflow-hidden flex flex-col gap-10"
       >
         <div className="absolute -top-8 -right-8 bg-gradient-to-b from-white to-[#BDCFF5] opacity-30 rounded-full blur-3xl pointer-events-none"></div>
         <div className="flex items-start justify-between z-10 relative w-full">
@@ -33,21 +33,21 @@ const Social = () => {
         href="mailto:aliihooshangi@gmail.com"
         rel="noopener noreferrer"
         aria-label="send me a message in email"
-        className="w-[41.4%] relative z-20 bg-gradient-to-b from-[#35A5EE] to-[#1264D6] rounded-[35px] p-6 overflow-hidden flex flex-col gap-10 justify-between"
+        className="w-[44%] relative z-20 bg-gradient-to-r from-[#802e2e] to-[#710e0e] rounded-[35px] p-6 overflow-hidden flex flex-col gap-10 justify-between"
       >
-        <div className="absolute -top-8 -right-8 bg-gradient-to-r from-white to-[#ABD5FF] opacity-20 rounded-full blur-3xl pointer-events-none"></div>
+        <div className="absolute -top-8 -right-8 bg-gradient-to-r from-white to-[#FFD1E3] opacity-20 rounded-full blur-3xl pointer-events-none"></div>
         <div className="flex items-start justify-between z-10 relative w-full">
           <Image
-            src={FacebookIcon}
+            src={InstagramIcon}
             alt="mail icon"
             aria-label="mail icon"
             width={1080}
-            className="w-28 z-10 relative"
+            className="w-12 z-10 relative"
             loading="lazy"
           />
         </div>
         <div className="-mt-16 ps-1 z-20 relative flex flex-col gap-2 w-full">
-          <span className="text-base font-semibold">Facebook</span>
+          <span className="text-base font-semibold">Instagram</span>
           <h2 className="font-normal text-sm">@aliihooshangi</h2>
         </div>
       </Link>
