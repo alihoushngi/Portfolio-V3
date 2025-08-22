b5a5ac6f667ff7a7668de3b640cdb36416eb6328
Ali Houshangi <blumy.info@gmail.com>
2025-08-22 11:25:59 +0330

    project reset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,7 @@ export default function RootLayout({
         <meta name="theme-color" content="#317EFB" />
         <link rel="icon" href="/icons/icon-192x192.png" />
       </head>
-      <body className="overflow-hidden">
-        <div className="w-80 h-48 bg-[#F37261] absolute -left-28 -top-20 blur-[150px] rounded-full z-10"></div>
+      <body className="">
         <RegisterSW />
         <Suspense fallback={null}>
           <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
@@ -38,7 +37,6 @@ export default function RootLayout({
           </ThemeProvider>
         </Suspense>
         <Toaster />
-        <div className="w-full h-14 bg-[#842CF2] absolute -bottom-10 blur-[100px] rounded-full z-10"></div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,3 @@
-import About from "@/components/ui/About/About";
-import Info from "@/components/ui/Info/info";
-import Project from "@/components/ui/Project/Project";
-import Social from "@/components/ui/Social/Social";
-
 export default function Home() {
-  return (
-    <div className="flex flex-col justify-start items-start px-16 py-12 min-h-screen gap-6">
-      <div className="flex justify-start items-stretch w-full gap-6">
-        <Info />
-        <Social />
-        <About />
-      </div>
-      <div className="flex justify-start items-start w-full gap-6">
-        <Project />
-        <div className="w-1/2 flex gap-6">
-          <div className="bg-gradient-to-bl from-[#221d37] to-[#0a0920] px-12 py-6 relative z-20 rounded-[40px] flex flex-col justify-center items-center w-1/2 h-[32vh]"></div>
-          <div className="bg-gradient-to-r from-[#0f2723] to-[#102925] px-12 py-6 relative z-20 rounded-[40px] flex flex-col justify-center items-center w-1/2 h-[32vh]"></div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className=""></div>;
 }
