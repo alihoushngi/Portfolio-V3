920b8b50612d4c65cecffb303599871ffb161dbf
Ali Houshangi <blumy.info@gmail.com>
2025-08-23 22:19:28 +0330

    mobile footer done

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,9 +1,11 @@
 import FooterDesktop from "@/components/shared/Footer/components/FooterDesktop";
+import FooterMobile from "@/components/shared/Footer/components/FooterMobile";
 
 const Footer = () => {
   return (
     <div className="w-full">
       <FooterDesktop />
+      <FooterMobile />
     </div>
   );
 };
--- src/components/shared/Footer/components/FooterMobile.tsx
+++ src/components/shared/Footer/components/FooterMobile.tsx
@@ -1,5 +1,36 @@
+import Link from "next/link";
+import { BiLogoGmail } from "react-icons/bi";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa6";
+
 const FooterMobile = () => {
-  return <div></div>;
+  return (
+    <div className="md:hidden w-full border-t border-Appearance-Slate-700 text-Appearance-Slate-400 text-1 flex justify-between items-center">
+      <span className="border-r border-Appearance-Slate-700 py-4 px-7 text-nowrap">
+        find me in:
+      </span>
+      <Link
+        href="https://www.linkedin.com/in/alihoushangi/"
+        target="_blank"
+        className="text-3 text-Appearance-Slate-400 border-r border-Appearance-Slate-700 transition-all duration-200 ease-in-out hover:text-Primary-Orange-300_Main w-full text-center flex justify-center items-center py-4"
+      >
+        <FaLinkedinIn />
+      </Link>
+      <Link
+        href="mailto:aliihooshangi@gmail.com"
+        target="_blank"
+        className="text-3 text-Appearance-Slate-400 border-r border-Appearance-Slate-700 transition-all duration-200 ease-in-out hover:text-Primary-Orange-300_Main w-full text-center flex justify-center items-center py-4"
+      >
+        <BiLogoGmail />
+      </Link>
+      <Link
+        href="https://github.com/alihoushngi"
+        target="_blank"
+        className="text-3 text-Appearance-Slate-400 transition-all duration-200 ease-in-out hover:text-Primary-Orange-300_Main w-full text-center flex justify-center items-center py-4"
+      >
+        <FaGithub />
+      </Link>
+    </div>
+  );
 };
 
 export default FooterMobile;
