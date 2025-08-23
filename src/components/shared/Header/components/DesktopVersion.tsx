886a2963d27d24067a89da0ced474afd3e6a9b9b
Ali Houshangi <blumy.info@gmail.com>
2025-08-23 21:19:59 +0330

    padding desktop size change

--- src/components/shared/Header/components/DesktopVersion.tsx
+++ src/components/shared/Header/components/DesktopVersion.tsx
@@ -9,7 +9,7 @@ const DesktopVersion = ({ pathname }: IHeaderProps) => {
   return (
     <div className="max-md:hidden w-full border-b border-Appearance-Slate-700 text-Appearance-Slate-400 text-1 flex justify-between items-center">
       <div className="w-full flex justify-start items-center gap-24">
-        <h1 className="py-5 px-7">ali hooshangi</h1>
+        <h1 className="py-4 px-7">ali hooshangi</h1>
         <div className="flex justify-center items-center">
           {firstMenuItems.map((item, index) => {
             if (!item.disable) {
@@ -17,7 +17,7 @@ const DesktopVersion = ({ pathname }: IHeaderProps) => {
                 <Link
                   href={item.url}
                   key={index}
-                  className={`${pathname === item.url && "border-b-2 border-b-Primary-Orange-300_Main"} transition-all duration-200 ease-in-out py-5 px-7 border-r border-l border-Appearance-Slate-700`}
+                  className={`${pathname === item.url && "border-b-2 border-b-Primary-Orange-300_Main"} transition-all duration-200 ease-in-out py-4 px-7 border-r border-l border-Appearance-Slate-700`}
                 >
                   {item.title}
                 </Link>
@@ -33,7 +33,7 @@ const DesktopVersion = ({ pathname }: IHeaderProps) => {
               <Link
                 href={item.url}
                 key={index}
-                className={`${pathname === item.url && "border-b-2 border-b-Primary-Orange-300_Main"} transition-all duration-200 ease-in-out py-5 px-7 border-r border-l border-Appearance-Slate-700`}
+                className={`${pathname === item.url && "border-b-2 border-b-Primary-Orange-300_Main"} transition-all duration-200 ease-in-out py-4 px-7 border-l border-Appearance-Slate-700`}
               >
                 {item.title}
               </Link>
