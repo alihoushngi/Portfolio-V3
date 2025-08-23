80e3259854897f6bb93c398b45d2ed5b3dd9a473
Ali Hooshangi <blumy.info@gmail.com>
2025-08-23 22:20:30 +0330

    Footer (#10)
    * footer desktop done
    
    * mobile footer done

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,14 @@
+import Footer from "@/components/shared/Footer/Footer";
 import Header from "@/components/shared/Header/Header";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="p-7 max-md:p-5 bg-Appearance-Slate-950 h-screen">
-      <div className="border border-Appearance-Slate-700 h-full rounded-default">
+      <div className="border border-Appearance-Slate-700 h-full rounded-default flex flex-col justify-between">
         <Header />
-        {children}
+        <main className="h-full">{children}</main>
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/shared/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import FooterDesktop from "@/components/shared/Footer/components/FooterDesktop";
+import FooterMobile from "@/components/shared/Footer/components/FooterMobile";
+
+const Footer = () => {
+  return (
+    <div className="w-full">
+      <FooterDesktop />
+      <FooterMobile />
+    </div>
+  );
+};
+
+export default Footer;
