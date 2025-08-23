c62045f3010cbb63901e3883dd3619e346741914
Ali Houshangi <blumy.info@gmail.com>
2025-08-23 20:53:36 +0330

    mobile header with framer motion done

--- src/components/shared/Header/components/MobileVersion.tsx
+++ src/components/shared/Header/components/MobileVersion.tsx
@@ -1,27 +1,57 @@
 import { IHeaderProps } from "@/components/shared/Header/assets/header.type";
+import { firstMenuItems } from "@/components/shared/Header/assets/items";
+import { AnimatePresence, motion } from "framer-motion";
+import Link from "next/link";
 import { useState } from "react";
-import { IoIosMenu } from "react-icons/io";
+import { IoIosMenu, IoMdClose } from "react-icons/io";
 
 const MobileVersion = ({ pathname }: IHeaderProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  console.log(pathname);
   return (
     <div className="md:hidden flex justify-between items-center text-Appearance-Slate-400 text-1 w-full border-b border-Appearance-Slate-700 p-5">
       <h1 className="">ali hooshangi</h1>
-      <IoIosMenu className="" onClick={() => setIsOpen(true)} />
+      <IoIosMenu className="text-4" onClick={() => setIsOpen(true)} />
 
       {/* Menu */}
-      {isOpen && (
-        <div className="fixed top-0 left-0 right-0 w-full bg-Appearance-Slate-950 h-screen z-10 p-5">
-          <div className="border border-Appearance-Slate-700 h-full rounded-default">
-            <div className="flex justify-between items-center text-Appearance-Slate-400 text-1 w-full border-b border-Appearance-Slate-700 p-5">
-              <h1 className="">ali hooshangi</h1>
-              <IoIosMenu className="" onClick={() => setIsOpen(true)} />
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: "96vh", opacity: 1 }}
+            exit={{ height: 0, opacity: 0 }}
+            transition={{ duration: 0.4, ease: "easeInOut" }}
+            className="fixed top-5 left-5 right-5 bottom-5 bg-Appearance-Slate-950 z-10 overflow-hidden"
+          >
+            <div className="border border-Appearance-Slate-700 h-full rounded-default flex flex-col justify-start items-center">
+              <div className="flex justify-between items-center text-Appearance-Slate-400 text-1 w-full border-b border-Appearance-Slate-700 p-5">
+                <h1 className="">ali hooshangi</h1>
+                <IoMdClose
+                  className="text-4"
+                  onClick={() => setIsOpen(!isOpen)}
+                />
+              </div>
+              <span className="w-full pb-4 pt-6 px-6 border-b border-Appearance-Slate-700 text-Appearance-Slate-400">
+                # navigate:
+              </span>
+              {firstMenuItems.map((item, index) => {
+                if (!item.disable) {
+                  return (
+                    <Link
+                      href={item.url}
+                      key={index}
+                      className={`${pathname === item.url && "border-b-2 border-b-Primary-Orange-300_Main"} w-full transition-all duration-200 ease-in-out py-4 px-6 border-b border-Appearance-Slate-700 text-white`}
+                      onClick={() => setIsOpen(!isOpen)}
+                    >
+                      {item.title}
+                    </Link>
+                  );
+                }
+              })}
             </div>
-          </div>
-        </div>
-      )}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
