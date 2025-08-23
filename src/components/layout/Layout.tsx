bc1388bdc27bd8dbc3b13c0ee70715f3e0a0aca6
Ali Houshangi <blumy.info@gmail.com>
2025-08-23 21:03:40 +0330

    layout padding change

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="p-11 max-md:p-5 bg-Appearance-Slate-950 h-screen">
+    <div className="p-7 max-md:p-5 bg-Appearance-Slate-950 h-screen">
       <div className="border border-Appearance-Slate-700 h-full rounded-default">
         <Header />
         {children}
