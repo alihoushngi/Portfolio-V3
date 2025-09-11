85843f68e5754cf2a88c92f0a52033a65c7cb396
Ali Houshangi <blumy.info@gmail.com>
2025-09-12 02:18:40 +0330

    image card done

--- src/components/ui/ImageCard/ImageCard.tsx
+++ src/components/ui/ImageCard/ImageCard.tsx
@@ -4,7 +4,7 @@ import { FC } from "react";
 
 const ImageCard: FC<IImageProps> = ({ ImageSrc, ImageAlt, ImageAriaLabel }) => {
   return (
-    <div className="flex flex-col w-[48%] justify-start items-start gap-4 bg-Appearance-Slate-700 rounded-lg p-4 hover:bg-Appearance-Slate-800 transition-all ease-linear duration-200 max-md:gap-2">
+    <div className="flex flex-col w-[48%] max-md:w-full justify-start items-start gap-4 bg-Appearance-Slate-700 rounded-lg p-4 hover:bg-Appearance-Slate-800 transition-all ease-linear duration-200 max-md:gap-2">
       <Image
         src={ImageSrc}
         alt={ImageAlt}
