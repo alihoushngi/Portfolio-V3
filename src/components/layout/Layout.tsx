6941cc7ba0ff5db50a3aa2cf51681bc4831cb810
Ali Houshangi <blumy.info@gmail.com>
2025-09-17 22:39:30 +0330

    height change

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="p-5 max-md:p-5 bg-Appearance-Slate-950 min-h-screen h-screen max-md:h-full w-full">
+    <div className="p-5 max-md:p-5 bg-Appearance-Slate-950 min-h-screen h-lvh max-md:h-full w-full">
       <div className="border border-Appearance-Slate-700 bg-Appearance-Slate-900 min-h-full h-full rounded-default flex flex-col justify-between">
         <Header />
         <main className="h-full w-full">{children}</main>
